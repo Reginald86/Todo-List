3aa8020f19ccbcc288ba82487e7f24eee98ceb2c
Reginald86 <49849893+Reginald86@users.noreply.github.com>
2025-08-24 13:52:06 -0500

    Use Project btns to select to current project and render tasks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import "./styles.css"
 import { TaskDialog } from "./tasks.js"
-import { createProject } from "./project.js";
+import { createProject} from "./project.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     TaskDialog();
     createProject();
 })
-
--- src/project.js
+++ src/project.js
@@ -2,10 +2,12 @@ import { deleteTask, reviseTask } from "./tasks.js";
 
 export let currentProject = null;
 export const projectArray = [];
+let count = 0;
 
 class Project {
-    constructor(name){
+    constructor(name, id){
         this.name = name;
+        this.id = id;
         this.tasks = [];
     }
     
@@ -18,12 +20,15 @@ export function createProject(){
     const projectName = document.getElementById("project-name");
     projectName.addEventListener("keydown", (e) => {
         if(e.key === "Enter"){
-            const project = new Project(projectName.value);
+            const project = new Project(projectName.value, count);
+            count++;
             projectName.value = "";
             currentProject = project;
             projectArray.push(project);
-            console.log(project)
+
+            console.log(currentProject);
             renderProject(currentProject);
+            renderTasks(currentProject);
         }
     })
 }
@@ -61,6 +66,27 @@ function renderProject (project) {
     const projectSelectBtn = document.createElement("button")
     projectSelectBtn.className = "projects";
     projectSelectBtn.textContent = project.name;
+    projectSelectBtn.id = project.id;
     projectContainer.append(projectSelectBtn);
+    activeProject();
+    
 }
 
+export function activeProject() {
+    document.querySelectorAll(".projects").forEach(project => {
+        project.addEventListener("click", (e) => {
+            console.log(typeof(project.id));
+            console.log(`${currentProject.id} is current`)
+            console.log(projectArray)
+
+            //find index of Project in projectArray associated with projectSelectBtn
+            const index = projectArray.findIndex(obj => obj.id.toString() === project.id);
+            
+            if(index !== -1 && currentProject.id !== project.id.toString()){
+                currentProject = projectArray[index];
+                console.log(currentProject);
+                renderTasks(currentProject);
+            }
+    });
+    })
+}
